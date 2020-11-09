8bdbc7284f1028094c21358fa5df3c0381e227b1
Erling Staff <erlingens@gmail.com>
2020-11-09 19:17:28 +0100

    hr clarification and complete button visibility checks

--- reasonJs.js
+++ reasonJs.js
@@ -1,8 +1,18 @@
+var number = 0;
+
 function clicked(tagNr){
     if (tagNr.style.border == "1px solid black"){
         tagNr.style.border = "";
+        number = number-1;
     }
     else{
         tagNr.style.border = "1px solid black";
+        number = number+1;
+    }
+    if (number > 0 ){
+        document.getElementById("pwBtn2").style.display="block";
+    }
+    else{
+        document.getElementById("pwBtn2").style.display="none";
     }
 }
